ccbfa2b5eb2d972a7908a69c3df5fe25b616981b
Aryan Kumar Dubey <96795631+Aryan-we@users.noreply.github.com>
2025-10-10 22:11:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,110 +1,71 @@
+let matchInput = prompt("Enter the number of matches to play\nClick Cancel to exit the game");
 
-let user=0;
-let computer=0;
-document.getElementById("computerScore").innerText="0"; 
-document.getElementById("myScore").innerText="0"; 
-function myFunc1(){
-    let a=Math.random();
-    let b=a*3;
-    let c=Math.floor(b);
-    let rocks=document.getElementById("userImage").src="./photo/rock.jpg";
-    if(c==0){
-        rock=document.getElementById("computerImage").src="./photo/rock.jpg";
-        if(rocks && rock){
-            document.getElementById("result").innerHTML="Match Draw"; 
-            let audio=new Audio("./sound/draw.mp3");
-            audio.play();           
+if (matchInput === null) {
+    window.close();
+} else if (!/^\d+$/.test(matchInput)) {
+    alert("Enter a valid number");
+    document.location.reload();
+} else {
+    let match = parseInt(matchInput);
+    if (match <= 0) {
+        alert("Invalid Number");
+        document.location.reload();
+    }
+
+    let m_count = -1;
+    let user = 0;
+    let computer = 0;
+
+    document.getElementById("match").innerHTML = match;
+    document.getElementById("computerScore").innerText = "0";
+    document.getElementById("myScore").innerText = "0";
+
+    function endGame() {
+        if (m_count === match) {
+            if (user > computer) {
+                alert("User Wins!");
+            } else if (user < computer) {
+                alert("Computer Wins!");
+            } else {
+                alert("Match Draw!");
+            }
+            window.location.reload();
         }
     }
-    if(c==1){
-        paper=document.getElementById("computerImage").src="./photo/paper.jpg";
-        if(rocks && paper){
-            document.getElementById("result").innerHTML="User Loss"; 
-            computer+=1;
-            document.getElementById("computerScore").innerText=computer;  
-            let audio=new Audio("./sound/loss.wav");
-            audio.play(); 
+
+    function playRound(userChoice) {
+        if (m_count >= match) return;
+
+        let c = Math.floor(Math.random() * 3);
+        const choices = ["rock", "paper", "scissor"];
+
+        document.getElementById("userImage").src = `./photo/${userChoice}.jpg`;
+        document.getElementById("computerImage").src = `./photo/${choices[c]}.jpg`;
+
+        if (userChoice === choices[c]) {
+            document.getElementById("result").innerHTML = "Match Draw";
+            new Audio("./sound/draw.mp3").play();
+        } else if (
+            (userChoice === "rock" && choices[c] === "scissor") ||
+            (userChoice === "paper" && choices[c] === "rock") ||
+            (userChoice === "scissor" && choices[c] === "paper")
+        ) {
+            document.getElementById("result").innerHTML = "User Wins";
+            user++;
+            document.getElementById("myScore").innerText = user;
+            new Audio("./sound/win.wav").play();
+        } else {
+            document.getElementById("result").innerHTML = "User Loss";
+            computer++;
+            document.getElementById("computerScore").innerText = computer;
+            new Audio("./sound/loss.wav").play();
         }
+
+        m_count++;
+        endGame();
     }
-    if(c==2){
-        scissor=document.getElementById("computerImage").src="./photo/scissor.jpg";
-        if(rocks && scissor){
-            document.getElementById("result").innerHTML="User Winner";  
-            user+=1;
-            document.getElementById("myScore").innerText=user;  
-            let audio=new Audio("./sound/win.wav");
-            audio.play();          
-        }
-    }
+
+    window.myFunc1 = () => playRound("rock");
+    window.myFunc2 = () => playRound("paper");
+    window.myFunc3 = () => playRound("scissor");
 }
-function myFunc2(){
-    let a=Math.random();
-    let b=a*3;
-    let c=Math.floor(b);
-    let papers=document.getElementById("userImage").src="./photo/paper.jpg";
-    if(c==0){
-        rock=document.getElementById("computerImage").src="./photo/rock.jpg";
-        if(papers && rock){
-            document.getElementById("result").innerHTML="User Winner";      
-            user+=1;
-            document.getElementById("myScore").innerText=user;    
-            let audio=new Audio("./sound/win.wav");
-            audio.play();          
-            
-        }
-    }
-    if(c==1){
-        paper=document.getElementById("computerImage").src="./photo/paper.jpg";
-        if(papers && paper){
-            document.getElementById("result").innerHTML="Match Draw";    
-            let audio=new Audio("./sound/draw.mp3");
-            audio.play();           
-        }
-    }
-    if(c==2){
-        scissor=document.getElementById("computerImage").src="./photo/scissor.jpg";
-        if(papers && scissor){
-            document.getElementById("result").innerHTML="User Loss";     
-            computer+=1;
-            document.getElementById("computerScore").innerText=computer;    
-            let audio=new Audio("./sound/loss.wav");
-            audio.play();        
-        }
-    }
-    }
-function myFunc3(){
-    let a=Math.random();
-    let b=a*3;
-    let c=Math.floor(b);
-    let scissors=document.getElementById("userImage").src="./photo/scissor.jpg";
-    if(c==0){
-        rock=document.getElementById("computerImage").src="./photo/rock.jpg";
-        if(scissors && rock){
-            document.getElementById("result").innerHTML="User Loss";   
-            computer+=1;
-            document.getElementById("computerScore").innerText=computer;   
-            let audio=new Audio("./sound/loss.wav");
-            audio.play();           
-        }
-    }
-    if(c==1){
-        paper=document.getElementById("computerImage").src="./photo/paper.jpg";
-        if(scissors && paper){
-            document.getElementById("result").innerHTML="User Winner";    
-            user+=1;
-            document.getElementById("myScore").innerText=user;  
-            let audio=new Audio("./sound/win.wav");
-            audio.play();          
-                
-        }
-    }
-    if(c==2){
-        scissor=document.getElementById("computerImage").src="./photo/scissor.jpg";
-        if(scissors && scissor){
-            document.getElementById("result").innerHTML="Match draw";   
-            let audio=new Audio("./sound/draw.mp3");
-            audio.play();            
-        }
-    }
-    }
-
